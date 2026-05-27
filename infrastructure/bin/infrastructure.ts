0db11fe290637e123bfa8da6ff509e7612f827fa
Sebexe <sebyex304@gmail.com>
2026-05-27 19:34:29 -0300

    Arreglado bug de la region

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -3,11 +3,19 @@ import * as cdk from 'aws-cdk-lib';
 import { BackendStack } from '../lib/backend-stack';
 import { FrontendStack } from '../lib/frontend-stack';
 
+declare const process: {
+	env: {
+		CDK_DEFAULT_ACCOUNT?: string;
+		CDK_DEFAULT_REGION?: string;
+	};
+};
+
 const app = new cdk.App();
-new BackendStack(app, 'BackendStack', {
+const env = {
+	account: process.env.CDK_DEFAULT_ACCOUNT,
+	region: process.env.CDK_DEFAULT_REGION,
+};
 
-});
+new BackendStack(app, 'BackendStack', { env });
 
-new FrontendStack(app, 'FrontendStack', {
-
-});
+new FrontendStack(app, 'FrontendStack', { env });
